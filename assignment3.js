c9939e817ffab873eac726608575070eb1a45295
LincolnLidanya <lincolnlidanya@gmail.com>
2020-07-02 01:09:09 +0300

    Add javascript functions

--- assignment3.js
+++ assignment3.js
@@ -37,3 +37,24 @@ $(document).ready(function () {
         }
     });
 });
+//main function that is called by form
+function myFunction() {
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var message=document.getElementById("message").value;
+
+  if (validateData(name,email,message)==true) {
+    alert("Thank you for contacting us "+name+". Your message is received. Our support team will get in touch with you");
+  } else {
+      alert ("Sorry, please capture all information on needed on the contact form.")
+  }
+
+}
+//function to validate details entered
+function validateData (name,email,message) {
+   var result=true;
+   if (name=="" || email=="" || message=="")  {
+        result=false;
+   }
+   return result;
+}
